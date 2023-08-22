33cf50bfdd53dff5dfe09738d6e99f2fbb3efa71
ankity26 <ankity2608@gmail.com>
2023-08-22 14:38:22 +0530

    Piece class updated

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -9,7 +9,7 @@ public abstract class Tile {
 //chess tile class.
     protected final int tileCoordinate;
 
-    private static final Map<Integer , EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+    private static final Map<Integer , EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
 
     private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
         final Map<Integer , EmptyTile> emptyTileMap = new HashMap<>();
@@ -21,7 +21,7 @@ public abstract class Tile {
     }
 
     public static Tile createTile(final int tileCoordinate , final Piece piece){
-        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
     }
 
     private Tile(int tileCoordinate){
